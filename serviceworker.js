60c0cf6b974f10bec4f5396b71acdacd8ee2e052
Rajeev Thomas <126510270+iamrajeevthomas@users.noreply.github.com>
2026-03-03 21:50:40 +0530

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,57 @@
+// Service Worker for Travel Booking App
+const CACHE_NAME = 'travel-booking-cache-v1';
+
+// Files to cache
+const urlsToCache = [
+  '/',
+  '/static/css/',
+  '/static/js/',
+  '/static/images/'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        
+        // Clone the request
+        const fetchRequest = event.request.clone();
+        
+        return fetch(fetchRequest).then(
+          response => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // Clone the response
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          }
+        );
+      })
+  );
+}); 
